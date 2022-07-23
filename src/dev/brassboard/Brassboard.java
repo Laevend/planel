3de49b7aa8d754df34831efefe8c1a2a934f89c2
yukiemeralis <yukiemeralis@gmail.com>
2022-07-23 08:30:57 -0600

    Plugin framework

---
+++ src/dev/brassboard/Brassboard.java
@@ -0,0 +1,18 @@
+package dev.brassboard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Brassboard extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
